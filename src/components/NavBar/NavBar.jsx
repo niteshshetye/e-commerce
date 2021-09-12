17d362d0f8b5b8396adb3e907c49a092ee12eba2
nitesh shetye <niteshshetye@nimapinfotech.com>
2021-09-12 12:41:15 +0530

    change in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
                 <p className="navbar-brand">Nit - Shoping Center</p>
                 <form className="d-flex">
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
+                    <button className="btn btn-outline-success" type="submit">Search Product</button>
                 </form>
             </div>
         </nav>
